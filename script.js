efe60b4b97e331e5fbade9239cebff02bb19eefa
Swift <154719586+Swiftsaa@users.noreply.github.com>
2024-02-21 19:31:14 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+const form = document.getElementById('form');
+
+form.addEventListener('submit', function(event) {
+    // Prevent the default form submission behavior
+    event.preventDefault();
+
+    const num1 = parseFloat(document.getElementById("num1").value);
+    const num2 = parseFloat(document.getElementById("num2").value);
+    const num3 = parseFloat(document.getElementById("num3").value);
+    const num4 = parseFloat(document.getElementById("num4").value);
+    const result = document.getElementById("result")
+
+
+    c1 = num2 - num1;
+    c2 = num3 - num2;
+    c3 = num4 - num3;
+    console.log(num1, num2, num3, num4)
+
+    if (c1 == c2 && c1 === c3 && c2 == c3) {
+        result.textContent = "You are Arthmetic";
+    } else {
+        result.textContent = "You are NOT Arthmetic";
+    }
+});
